44283cf8d5f1833a8138a1029288170c2e247ba8
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-04-03 16:40:22 +0800

    Create parser functions

--- parser.go
+++ parser.go
@@ -1,9 +1,9 @@
 package linkparser
 
 import (
-	"io"
 	"golang.org/x/net/html"
-	"fmt"
+	"io"
+	"strings"
 )
 
 // Link represents an HTML link (anchor tag).
@@ -18,20 +18,51 @@ func Parse(r io.Reader) ([]Link, error) {
 	if err != nil {
 		panic(err)
 	}
-	dfs(doc, "")
-	return nil, nil
+	var links []Link
+	nodes := getLinkNodes(doc)
+	for _, node := range nodes {
+		links = append(links, buildLink(node))
+	}
+	return links, nil
 }
 
-func dfs(n *html.Node, padding string) {
-	if n.Type == html.ElementNode && n != nil {
-		fmt.Printf("%s<%s>\n", padding, n.Data)	
+// Builds a Link from an HTML Node
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+// Parses an HTML Node and finds all the anchor tags from its children
+func getLinkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
 	}
 
-	if n != nil && n.Type == html.TextNode {
-		fmt.Println(padding, n.Data)
-	}
-
+	var ret []*html.Node
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding + "  ")
+		ret = append(ret, getLinkNodes(c)...)
 	}
+	return ret
+}
+
+// Build a string from an HTML nodes, recursively adding its children's text as well
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c)
+	}
+	return strings.Join(strings.Fields(ret), " ")
 }
