18c784c88b35b12d3c4bd1d3904f8fbb1b12bc52
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2020-04-03 16:45:21 +0800

    Add comment element

--- example/main.go
+++ example/main.go
@@ -12,7 +12,7 @@ func main() {
 <div>
     <h1>Example Data</h1>
     <p>Lorem ipsum <a href="/lorem">dolor</a></p>
-    <p><a href="/info">More information about our <span>services</span></a></p>
+    <p><a href="/info">More information about our <span>services</span> <!-- Some comment --></a></p>
 </div>
 </body>
 </html>
